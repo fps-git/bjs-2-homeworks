d6500c24d35f0c7ab7f65d641835fe5da00a73a6
fps-git <107875518+fps-git@users.noreply.github.com>
2023-07-09 21:44:18 +0200

    Homework 1.Base concepts

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,42 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = (b ** 2) - (4 * a * c);
+  if (d > 0) {
+    arr[0] = (-b + Math.sqrt(d)) / (2 * a);
+    arr[1] = (-b - Math.sqrt(d)) / (2 * a);
+  } else {
+    if (d === 0) {
+      arr[0] = -b / (2 * a);
+    }
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  contribution = Number(contribution);
+  if (isNaN(contribution)) {
+    return false;
+  }
+
+  amount = Number(amount);
+  if (isNaN(amount)) {
+    return false;
+  }
+
+  countMonths = Number(countMonths);
+  if (isNaN(countMonths)) {
+    return false;
+  }
+
+  let monthlyPercentage = Number(percent);
+  if (isNaN(monthlyPercentage)) {
+    return false;
+  } else {
+    monthlyPercentage = monthlyPercentage / 100 / 12;
+  }
+
+  let monthlyPayment = (amount - contribution) * (monthlyPercentage + (monthlyPercentage / (((1 + monthlyPercentage) ** countMonths) - 1)));
+  let wholePayment = ((monthlyPayment * countMonths)).toFixed(2);
+  return Number(wholePayment);
 }
