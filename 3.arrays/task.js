65ba577b0741cc1083a78765f35eeb3a997e6b7c
fps-git <107875518+fps-git@users.noreply.github.com>
2023-08-06 23:28:41 +0200

    Homework 3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,21 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length != arr2.length) {
+        return false;
+    } else {
+        if(arr1.every((element, index) => element === arr2[index])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let filtered = users.filter(user => user.gender === gender);
+    if (filtered.length == 0) {
+        return 0;
+    } else {
+        let result = filtered.reduce((a,b) => a + b.age, 0) / filtered.length;
+        return result;
+    }
 }
