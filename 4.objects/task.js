c4c39a840e19a92bdef14bbc5d35cf5f936a2fa6
fps-git <107875518+fps-git@users.noreply.github.com>
2023-08-08 00:39:01 +0200

    Homework 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,30 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = []; 
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (!!this.marks) {
+        this.marks = [...this.marks, ...marks];
+    }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (!!this.marks && this.marks.length != 0) {
+       return this.marks.reduce((a,b,index,arr) => a + b / arr.length, 0);
+    } else {
+        return 0;
+    }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
