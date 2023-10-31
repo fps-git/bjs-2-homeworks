9d67a0c5b422ee6c5ebccb2313ed26386e60b72c
fps-git <107875518+fps-git@users.noreply.github.com>
2023-10-31 01:11:32 +0100

    Homework decorators

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,40 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+    let cache = [{}, {}, {}, {}, {}];
+    return function(...args) {
+        const hash = md5(args);
+        let foundInCache = cache.find(cache => cache.hash === hash);
+        if (!!foundInCache) {
+            return "Из кэша: " + foundInCache.result;
+        }
+        const result = func(...args);
+        cache.shift();
+        let newCacheObj = {};
+        newCacheObj.hash = hash;
+        newCacheObj.result = result;
+        cache.push(newCacheObj);
+        return "Вычисляем: " + result;
+    }
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+    let timeoutId = null;
+    wrapper.allCount = 0;
+    wrapper.count = 0;
+    function wrapper(...args) {
+        wrapper.allCount++;
+
+        if (!timeoutId) {
+            func(...args);
+            wrapper.count++;
+        }
+
+        clearTimeout(timeoutId); 
+        timeoutId = setTimeout(() => {
+            wrapper.count++;
+            func(...args);
+        }, delay)  
+    }
+    return wrapper;
 }
